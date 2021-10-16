064b00651a8e8d9609c5f84bc3601634ad5f8589
Amit Bhalla <amit9815@gmail.com>
2021-10-16 10:11:16 +0530

    Reverse String

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,30 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  /* Solution 3 */
+  const arr = str.split('');
+  return arr.reduce((rev, char) => {
+    return char + rev;
+  }, '');
+}
 
 module.exports = reverse;
+
+// function reverse(str) {
+//   /* Solution 1 */
+//   const arr = str.split('');
+//   arr.reverse();
+//   return arr.join('');
+// }
+
+// --
+
+// function reverse(str) {
+//   /* Solution 2 */
+//   let reversed = '';
+//   for (let char of str) {
+//     reversed = char + reversed;
+//   }
+//   return reversed;
+// }
