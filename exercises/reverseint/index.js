14443c41a8cc1a48276f7758e000518b14d10e2c
Amit Bhalla <amit9815@gmail.com>
2021-10-16 17:45:29 +0530

    Reverse int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,18 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  //   const negative = n < 0;
+  //   const num = Math.abs(n);
+  //   const revNum = num.toString().split('').reverse().join('');
+  //   if (negative) {
+  //     return 0 - +revNum;
+  //   } else {
+  //     return +revNum;
+  //   }
+  return n < 0
+    ? 0 - +Math.abs(n).toString().split('').reverse().join('')
+    : +Math.abs(n).toString().split('').reverse().join('');
+}
 
 module.exports = reverseInt;
