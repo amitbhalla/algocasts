99a61926162f5ee709be56a7344648f32f929f75
Amit Bhalla <amit9815@gmail.com>
2021-10-16 18:50:39 +0530

    MaxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,20 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const obj = {};
+  let freq = 0;
+  let ans = '';
+  for (let char in str) {
+    obj[str[char]] ? obj[str[char]]++ : (obj[str[char]] = 1);
+  }
+  for (let prop in obj) {
+    if (freq < obj[prop]) {
+      freq = obj[prop];
+      ans = prop;
+    }
+  }
+  return ans;
+}
 
 module.exports = maxChar;
